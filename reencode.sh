67924ffbb71d3414476e2026a968215146e3e22f
Silviu Simen <silviu.simen@gmail.com>
2024-08-09 08:32:32 -0400

    Script updates

--- reencode.sh
+++ reencode.sh
@@ -18,8 +18,7 @@ if [ -f "$file" ] ; then
         fi
         if [ ! -f "$ofile" ] ; then
             echo ffmpeg -loglevel error -stats -i $file -map 0 -c:s copy -b:v ${VBPS} -b:a ${ABPS} "$ofile"
-            echo "---------------------------------------------"
-            # nice ffmpeg -loglevel error -stats -i "$file" -map 0 -c:s copy -b:v ${VBPS} -b:a ${ABPS} "$ofile"
+            nice ffmpeg -loglevel error -stats -i "$file" -map 0 -c:s copy -b:v ${VBPS} -b:a ${ABPS} "$ofile"
         else
             echo "Skipping processing $file since $ofile is already present"
         fi
